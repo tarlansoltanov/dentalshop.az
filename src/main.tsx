1f6de4730a0dc394f46066d82ccb2f9b46fcc3ac
Tarlan Soltanov <tarlansoltanov@outlook.com>
2024-01-16 15:57:50 +0100

    Redux and redux-toolkit added and configured

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,19 @@ import ReactDOM from "react-dom/client";
 // React Router
 import { BrowserRouter } from "react-router-dom";
 
+// Redux
+import { Provider } from "react-redux";
+import { store } from "@/store";
+
 // App
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
