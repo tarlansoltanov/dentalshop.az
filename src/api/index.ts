25cc2110380f3d8dc068afea5db365a4d7267c70
Tarlan Soltanov <tarlansoltanov@outlook.com>
2024-04-08 04:03:51 +0400

    Default Language added to API

--- src/api/index.ts
+++ src/api/index.ts
@@ -17,6 +17,9 @@ const API_URL = import.meta.env.VITE_API_URL as string;
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
+  headers: {
+    "Accept-Language": "az",
+  },
 });
 
 axiosInstance.interceptors.request.use(
