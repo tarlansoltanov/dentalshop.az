becb07e68aff50aff6ef57aff90772faa69ad999
nihad memmedzade <nihad.mmmdzad@bk.ru>
2024-08-10 11:53:23 +0400

    editedTimerUi

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -12,31 +12,51 @@ const Timer = ({ end_date, className }: Props) => {
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
+  // const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
       // Time for GMT+4
-      const now = new Date().getTime() + 14400000;
+      const now = new Date().getTime() ;   //+14400000
       const distance = date.getTime() - now;
 
       setDays(Math.floor(distance / (1000 * 60 * 60 * 24)));
       setHours(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
       setMinutes(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
-      setSeconds(Math.floor((distance % (1000 * 60)) / 1000));
+      // setSeconds(Math.floor((distance % (1000 * 60)) / 1000));
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [date]);
+
+  console.log(days,"hours");
+  
 
   return (
     <div className={className}>
-      <p>
-        {days ? `${days} gün ` : ""}
-        {hours ? `${hours} saat ` : ""}
-        {minutes ? `${minutes} dəqiqə ` : ""}
-        {seconds ? `${seconds} saniyə ` : ""}
-      </p>
+      <div className="containerData1">
+      <p className="timerTextStyle">
+      {days}</p>
+        <p className="timerTextStyle2">
+        Gün</p>
+        </div>
+<div className="lineData"></div>
+        <div className="containerData1">
+      <p className="timerTextStyle">
+      {hours ? `${hours}` : ""}</p>
+        <p className="timerTextStyle2">
+        {hours ? ` Saat` : ""}</p>
+        </div>
+        <div className="lineData"></div>
+
+        <div className="containerData1">
+      <p className="timerTextStyle">
+      {minutes ? `${minutes}` : ""}</p>
+        <p className="timerTextStyle2">
+        {minutes ? ` Dəqiqə` : ""}</p>
+        </div> 
+        {/* {seconds || minutes || hours || days ? `${seconds} saniyə ` : ""} */}
+  
     </div>
   );
 };
